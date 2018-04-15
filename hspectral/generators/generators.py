fec98c88ef9f2fb20fc0b8276931fefcd264dfe9
Pawel Ksieniewicz <pawel@ksienie.com>
2018-04-15 15:41:06 +0200

    Anomaly generator.

--- hspectral/generators/generators.py
+++ hspectral/generators/generators.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_anomaly(shape = (100, 100, 50), n_anomalies = 2, a_size = 10,
-                     random_state = None):
+                     random_state = None, noise_scale = .1):
     np.random.seed(random_state)
 
     # Assigning shape of image
@@ -20,9 +20,21 @@ def generate_anomaly(shape = (100, 100, 50), n_anomalies = 2, a_size = 10,
         y[positions_x[i]:positions_x[i]+a_size,
           positions_y[i]:positions_y[i]+a_size] = 1
 
-    print (positions_x)
-    print (positions_y)``
-    plt.imshow(y, cmap='gray')
-    plt.savefig('foo.png')
+    # Making class signatures
+    s_attr = np.linspace(0, 2*3.1415, shape[2])
+
+    a = .5 + np.random.random_sample()
+    b = .5 + np.random.random_sample()
+
+    anomaly_signature = (np.cos(a * s_attr) + 1)/2
+    background_signature = (np.sin(b * s_attr) + 1)/2
+
+    # Building image from Signatures
+    X[y==0] = background_signature
+    X[y==1] = anomaly_signature
+
+    # Creating noise
+    noise = np.abs(np.random.normal(0, noise_scale, shape))
+    X += noise
 
     return X, y
--- hspectral/tests/test_generators.py
+++ hspectral/tests/test_generators.py
@@ -1,8 +1,17 @@
 import sys
 from hspectral import generators
+import matplotlib.pyplot as plt
 sys.path.insert(0, '../..')
 
 
 def test_bare_controller():
-    X, y = generators.generate_anomaly(random_state=1)
-    assert(False)
+    X, y = generators.generate_anomaly(random_state=1, n_anomalies = 4,
+                                       noise_scale = .3)
+    fig, ax = plt.subplots(4,1, figsize=(2,8))
+    ax[0].imshow(y, cmap='gray')
+    ax[0].set_title('Ground truth')
+    ax[1].imshow(X[:,10,:], cmap='gray')
+    ax[2].imshow(X[:,:,20], cmap='gray')
+    ax[3].imshow(X[:,:,40], cmap='gray')
+
+    plt.savefig('foo.png')
